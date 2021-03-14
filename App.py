a576aaefa2ff35d734b0b5e7247b81af16a1e6a5
jitender.singh <jitender.singh@salesforce.com>
2021-03-14 17:07:23 +0530

    slightly better user interface with ttk widgets, default username option added.

--- App.py
+++ App.py
@@ -21,6 +21,7 @@ class App:
         self.pass_box = None
         self.url_box = None
         self.scrollable_frame = None
+        self.style = None
 
         self.progress_bar_values = list()
         self.download_buttons = list()
@@ -42,6 +43,12 @@ class App:
         pad = 3
         geometry = '{}x{}+0+0'.format(self.app.winfo_screenwidth()-pad, self.app.winfo_screenheight()-pad)
         self.app.geometry(geometry)
+
+        # style for buttons
+        style = ttk.Style()
+        style.map("TButton", foreground=[("active", "white"), ("disabled", "gray")])
+        self.style = style
+
         self._add_content(self.app)
         self.app.mainloop()
 
@@ -55,49 +62,70 @@ class App:
         """
         Adds authentication widgets and blank frame for holding video/lectures data.
         """
-        options = {
+        grid_options = {
             'padx': 5,
             'pady': 5,
         }
+        label_options = {
+            'padx': 5,
+            'pady': 5,
+            'sticky': tk.E,
+        }
+        entry_options = {
+            'padx': 5,
+            'pady': 5,
+            'sticky': tk.E,
+        }
+
         tk.Grid.rowconfigure(anchor, 0, weight=1)
         tk.Grid.columnconfigure(anchor, 0, weight=1)
-        root_url = self.impartus.conf.get('impartus_url')
         frame_auth = tk.Frame(anchor, width=1000, height=200, padx=10, pady=10, bg='grey')
-        frame_auth.grid(row=0, column=0, **options)
+        frame_auth.grid(row=0, column=0, **grid_options)
         self.frame_auth = frame_auth
 
         frame_videos = tk.Frame(anchor, width=1000, height=600, padx=10, pady=10)
-        frame_videos.grid(row=1, column=0, **options)
+        frame_videos.grid(row=1, column=0, **grid_options)
         self.frame_videos = frame_videos
 
-        tk.Label(frame_auth, borderwidth=1, text='URL').grid(row=0, column=0, **options)
-        url_box = tk.Entry(frame_auth, width=30)
-        url_box.insert(tk.END, root_url)
-        url_box.grid(row=0, column=1, **options)
+        # URL Label and Entry box.
+        ttk.Label(frame_auth, text='URL').grid(row=0, column=0, **label_options)
+        url_box = ttk.Entry(frame_auth, width=30)
+        url_box.insert(tk.END, self.impartus.conf.get('impartus_url'))
+        url_box.grid(row=0, column=1, **entry_options)
 
-        tk.Label(frame_auth, text='Username ').grid(row=1, column=0, **options)
-        user_box = tk.Entry(frame_auth, text='', width=30)
-        user_box.grid(row=1, column=1, **options)
+        # Username Label and Entry box.
+        ttk.Label(frame_auth, text='Username ').grid(row=1, column=0, **label_options)
+        user_box = ttk.Entry(frame_auth, width=30)
+        user_box.insert(tk.END, self.impartus.conf.get('username'))
+        user_box.grid(row=1, column=1, **entry_options)
 
-        tk.Label(frame_auth, text='Password ').grid(row=2, column=0, **options)
-        pass_box = tk.Entry(frame_auth, text='', show="*", width=30)
-        pass_box.grid(row=2, column=1, **options)
+        ttk.Label(frame_auth, text='Password ').grid(row=2, column=0, **label_options)
+        pass_box = ttk.Entry(frame_auth, text='', show="*", width=30)
+        pass_box.bind("<Return>", self.get_videos)
+        pass_box.grid(row=2, column=1, **entry_options)
 
-        tk.Button(frame_auth, text='List Videos', command=self.get_videos).grid(row=3, column=0, **options)
+        ttk.Button(frame_auth, text='Show Videos', command=self.get_videos).grid(row=3, column=0, **grid_options)
 
         self.user_box = user_box
         self.pass_box = pass_box
         self.url_box = url_box
+        # set focus to user entry if it is empty, else to password box.
+        if self.user_box.get() == '':
+            self.user_box.focus()
+        else:
+            self.pass_box.focus()
 
-    def get_videos(self):
+    def get_videos(self, event=None):
         """
         Callback function for 'List Videos' button.
         Fetch video/lectures list available to the user and display on the UI.
         """
+
         username = self.user_box.get()
         password = self.pass_box.get()
         root_url = self.url_box.get()
-
+        if username == '' or password == '':
+            return
         self.progress_bar_values.clear()
         self.download_buttons.clear()
         self.open_buttons.clear()
@@ -115,13 +143,15 @@ class App:
         frame = self.frame_videos
 
         # make it scrollable.
-        sf = ScrolledFrame(frame, width=1400, height=600)
+        sf = ScrolledFrame(frame, width=1400, height=600, use_ttk=True)
         sf.grid(row=0, column=0)
 
-        frame_table = sf.display_widget(tk.Frame)
         # Bind the arrow keys and scroll wheel
-        sf.bind_arrow_keys(frame_table)
-        sf.bind_scroll_wheel(frame_table)
+        sf.bind_arrow_keys(frame)
+        sf.bind_scroll_wheel(frame)
+
+        frame_table = sf.display_widget(tk.Frame)
+
         self.scrollable_frame = sf
 
         for i, col in enumerate(list(['Subject', 'Lecture No.', 'Professor', 'Topic', 'Date', 'Duration', 'Tracks', 'Downloaded?'])):
@@ -136,16 +166,19 @@ class App:
                     subject_name = video_metadata.get('subjectNameShort')
                 else:
                     subject_name = video_metadata.get('subjectName')
-                tk.Label(frame_table, text=subject_name).grid(row=row, column=0)
-                tk.Label(frame_table, text=video_metadata.get('seqNo')).grid(row=row, column=1)
-                tk.Label(frame_table, text=video_metadata.get('professorName')).grid(row=row, column=2)
-                tk.Label(frame_table, text=video_metadata.get('topic')).grid(row=row, column=3)
-                tk.Label(frame_table, text=video_metadata.get('startDate')).grid(row=row, column=4)
-                tk.Label(frame_table, text=video_metadata.get('actualDurationReadable')).grid(row=row, column=5)
-                tk.Label(frame_table, text=video_metadata.get('tapNToggle')).grid(row=row, column=6)
+                ttk.Label(frame_table, text=subject_name).grid(row=row, column=0)
+                ttk.Label(frame_table, text=video_metadata.get('seqNo')).grid(row=row, column=1)
+                ttk.Label(frame_table, text=video_metadata.get('professorName')).grid(row=row, column=2)
+                ttk.Label(frame_table, text=video_metadata.get('topic')).grid(row=row, column=3)
+                ttk.Label(frame_table, text=video_metadata.get('startDate')).grid(row=row, column=4)
+                ttk.Label(frame_table, text=video_metadata.get('actualDurationReadable')).grid(row=row, column=5)
+                ttk.Label(frame_table, text=video_metadata.get('tapNToggle')).grid(row=row, column=6)
                 filepath = self.impartus.get_mkv_path(video_metadata)
+                file_exists = os.path.exists(filepath)
+
+                # progress bar
                 progress_bar_value = tk.DoubleVar()
-                if os.path.exists(filepath):
+                if file_exists:
                     ttk.Progressbar(frame_table, orient=tk.HORIZONTAL, length=100, value=100,
                                     mode='determinate').grid(row=row, column=7)
                 else:
@@ -153,21 +186,26 @@ class App:
                                     variable=progress_bar_value, mode='determinate').grid(row=row, column=7)
                 self.progress_bar_values.append(progress_bar_value)
 
-                download_button = tk.Button(frame_table, text='Download', command=partial(
+                # download button
+                download_button = ttk.Button(frame_table, text='Download', command=partial(
                     self.download_video, video_metadata, filepath, root_url, row))
+                if os.path.exists(filepath):
+                    download_button.config(state='disabled')
                 download_button.grid(row=row, column=8)
                 self.download_buttons.append(download_button)
 
-                open_button = tk.Button(frame_table, text='Open', command=partial(Utils.open_file,
-                                                                                  os.path.dirname(filepath)))
+                # open button
+                open_button = ttk.Button(frame_table, text='Open', command=partial(
+                    Utils.open_file, os.path.dirname(filepath)))
                 if not os.path.exists(filepath):
-                    open_button.config(state=tk.DISABLED, disabledforeground="gray")
+                    open_button.config(state='disabled')
                 open_button.grid(row=row, column=9)
                 self.open_buttons.append(open_button)
 
-                play_button = tk.Button(frame_table, text='Play️', command=partial(Utils.open_file, filepath))
+                # play button
+                play_button = ttk.Button(frame_table, text='Play️', command=partial(Utils.open_file, filepath))
                 if not os.path.exists(filepath):
-                    play_button.config(state=tk.DISABLED, disabledforeground="gray")
+                    play_button.config(state='disabled')
                 play_button.grid(row=row, column=10)
                 self.play_buttons.append(play_button)
 
@@ -184,11 +222,9 @@ class App:
         imp.process_video(video_metadata, filepath, root_url, self.progress_bar_values[index-1])
 
         # download complete, enable open / play buttons
-        self.open_buttons[index-1].config(state=tk.NORMAL)
-        self.play_buttons[index-1].config(state=tk.NORMAL)
+        self.open_buttons[index-1].config(state='active')
 
-        # enable download button, for re-download if needed.
-        self.download_buttons[index - 1].config(state=tk.NORMAL)
+        self.play_buttons[index-1].config(state='active')
 
     def download_video(self, video_metadata, filepath, root_url, index):
         """
@@ -196,7 +232,7 @@ class App:
         Creates a thread to download the request video.
         """
         # disable download button.
-        self.download_buttons[index-1].config(state=tk.DISABLED, disabledforeground='gray')
+        self.download_buttons[index-1].config(state='disabled')
 
         thread = threading.Thread(target=self._download_video_thread, args=(video_metadata, filepath, root_url, index,))
         self.threads.append(thread)
