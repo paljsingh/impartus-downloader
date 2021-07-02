75b32289a47926e139d6118d4aa275b0220bc480
jitender.singh <jitender.singh@salesforce.com>
2021-07-03 00:12:06 +0530

    fixed slides dropdown icons with dark themes

--- ui/combobox.py
+++ ui/combobox.py
@@ -1,6 +1,8 @@
 import os
 
-from PySide2.QtWidgets import QComboBox
+from PySide2.QtCore import QEvent
+from PySide2.QtGui import QPalette
+from PySide2.QtWidgets import QComboBox, QApplication
 from typing import List
 import qtawesome as qta
 
@@ -17,7 +19,7 @@ class CustomComboBox(QComboBox):
         self.setMinimumWidth(100)
         self.setMaximumWidth(100)
 
-        # retain size when hidden
+        # retain size when hidden => do not shrink 'slides' tab when no slides dropdown present.
         retain_size_policy = self.sizePolicy()
         retain_size_policy.setRetainSizeWhenHidden(True)
         self.setSizePolicy(retain_size_policy)
@@ -25,6 +27,10 @@ class CustomComboBox(QComboBox):
         self.connection = None
 
     def add_items(self, items: List):
+        """
+        Add one or more items to the combo box.
+        Items in the input list are expected to be file paths.
+        """
         if not len(items):
             return
 
@@ -42,14 +48,36 @@ class CustomComboBox(QComboBox):
         for item in items:
             icon_name = SlideIcons.get_icon_type(item)
             self.addItem(qta.icon(icon_name), os.path.basename(item), item)
-            # self.insertItem(i, qta.icon(icon_name), os.path.basename(item), item)
-
-        print('item earlier {},  now {}'.format(prev_item_count, self.count()))
 
         if self.connection:
             self.activated.disconnect()
         self.activated.connect(self.show_slides)
         self.connection = True
 
+        self.apply_current_palette()
+
     def show_slides(self, index: int):    # noqa
+        """
+        Open the select lecture slides doc.
+        """
         Utils.open_file(self.itemData(index))
+
+    def changeEvent(self, event: QEvent) -> None:
+        """
+        On system theme change, update the progress bar colors.
+        """
+        super().changeEvent(event)
+        if event.type() == QEvent.PaletteChange:
+            self.apply_current_palette()
+
+    def apply_current_palette(self):
+        highlight_color = QApplication.palette().color(QPalette.Highlight)
+        text_color = QApplication.palette().color(QPalette.Text)
+
+        for i in range(self.count()):
+            qicon = qta.icon(
+                SlideIcons.get_icon_type(self.itemData(i)),
+                color=text_color,
+                color_active=highlight_color,
+            )
+            super().setItemIcon(i, qicon)
