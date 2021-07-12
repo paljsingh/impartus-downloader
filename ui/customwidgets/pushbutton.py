68f2b020a79aca8d287cb67f8e2ae4f319921f97
jitender.singh <jitender.singh@salesforce.com>
2021-07-12 21:17:26 +0530

    #97 - using spin icon internal to stop the timer.

--- ui/customwidgets/pushbutton.py
+++ ui/customwidgets/pushbutton.py
@@ -7,6 +7,7 @@ import qtawesome as qta
 class CustomPushButton(QPushButton):
     def __init__(self):
         super().__init__()
+        self.spin_icon = None
         pass
 
     def changeEvent(self, event: QEvent) -> None:
@@ -26,11 +27,12 @@ class CustomPushButton(QPushButton):
         text_color = QApplication.palette().color(QPalette.Text)
 
         if animate:
+            self.spin_icon = qta.Spin(self, interval=100, step=2)
             qicon = qta.icon(
                 self.objectName(),
                 color=text_color,
                 color_active=highlight_color,
-                animation=qta.Spin(self, interval=100, step=2)
+                animation=self.spin_icon,
             )
         else:
             qicon = qta.icon(
@@ -38,4 +40,9 @@ class CustomPushButton(QPushButton):
                 color=text_color,
                 color_active=highlight_color,
             )
+            if self.spin_icon:
+                # hacky-ish way to stop the spin icon from updating
+                # till the issue is fixed. https://github.com/spyder-ide/qtawesome/issues/165
+                self.spin_icon.info[self][0].stop()
+                self.spin_icon = None
         super().setIcon(qicon)
