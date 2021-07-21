361b1f80f81b4520015cdf818f11d4de792a774a
jitender.singh <jitender.singh@salesforce.com>
2021-07-21 21:11:41 +0530

    log thread exit status

--- ui/worker.py
+++ ui/worker.py
@@ -10,10 +10,11 @@ class Worker(QtCore.QThread):
         super().__init__(parent)
         self.task = None
         self.log_handler = ThreadLogHandler()
+        self.status = None
 
     def set_task(self, func: Callable):
         self.task = func
 
     def run(self):
         if self.task:
-            self.task()
+            self.status = self.task()
