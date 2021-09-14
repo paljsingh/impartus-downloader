cf09382fcffb721776bcc3c3e51c2f7f96f05746
jitender.singh <jitender.singh@salesforce.com>
2021-09-14 23:57:46 +0530

    show logs on console as well as ui window

--- lib/threadlogging.py
+++ lib/threadlogging.py
@@ -30,6 +30,22 @@ class ThreadLogHandler(logging.Handler):
         Variables().log_window().centerCursor()  # scroll to the bottom
 
 
+class ConsoleLogHandler(logging.Handler):
+
+    def __init__(self):
+        super().__init__()
+        self.model = Model()
+
+    # logging.Handler.emit() is intended to be implemented by subclasses
+    def emit(self, record):
+        msg = self.format(record)
+        self.model.update.emit(msg)     # noqa
+
+    @QtCore.Slot(str)       # noqa
+    def write_log(self, log_text):
+        print(log_text)
+
+
 class ThreadLogger:
 
     loggers = dict()
@@ -38,24 +54,29 @@ class ThreadLogger:
 
         # create logger for this class, and add it to a class dict.
         self.logger = logging.getLogger(logger_name)
+        self.logger.setLevel(logging.DEBUG)
         self.__class__.loggers[logger_name] = self.logger
+
+        if not self.logger.hasHandlers():
+            self.add_handler(self.logger, ConsoleLogHandler())
 
         # we want to direct all the logs to a ui text edit, which may not be available for till the content window
         # is drawn.
         # collect all the logger objects that are returned before creation of content_window
         # once the log window is available, map all the loggers with a handler.
-        if Variables().log_window():
-            for name, logger in self.__class__.loggers.items():
-                if not logger.hasHandlers():
-                    self.add_handler(logger)
+        logger: logging.Logger
+        for name, logger in self.__class__.loggers.items():
+            # add threadlog handler only if the content window is available.
+            if Variables().log_window():
+                self.add_handler(logger, ThreadLogHandler())
 
-    def add_handler(self, logger):  # noqa
+    def add_handler(self, logger, handler):  # noqa
         # set up log handler
-        log_handler = ThreadLogHandler()
-        log_handler.setFormatter(logging.Formatter('%(asctime)s - %(levelname)s - %(name)s - %(message)s'))
-        # set the logging level
-        logger.setLevel(logging.DEBUG)
+        # log_handler =
+
+        # for handler in [log_handler, console_handler]:
+        handler.setFormatter(logging.Formatter('%(asctime)s - %(levelname)s - %(name)s - %(message)s'))
 
         # emit signal and write to app log window.
-        log_handler.model.update.connect(log_handler.write_log)     # noqa
-        logger.addHandler(log_handler)
+        handler.model.update.connect(handler.write_log)     # noqa
+        logger.addHandler(handler)
