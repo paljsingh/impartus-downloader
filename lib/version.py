38e2e7fc1a81f745505058887b18ae2558b3d6bd
Jitender Singh <paljsingh@gmail.com>
2021-11-08 17:57:49 +0530

    Update version.py

--- lib/version.py
+++ lib/version.py
@@ -2,4 +2,4 @@
 current application version.
 Used by 'Help -> Check for updates' to compare against the latest version on github.
 """
-__version_info__ = 'v4.0-beta.11'
+__version_info__ = 'v4.0-beta.12'
